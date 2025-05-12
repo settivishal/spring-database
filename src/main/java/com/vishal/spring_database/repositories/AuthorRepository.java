b0ac5e55ee96695ef4dc9d407c78c05961d304b9
settivishal <vishalsetti2000@gmail.com>
2025-05-11 23:27:59 -0400

    Bug fixed

--- src/main/java/com/vishal/spring_database/repositories/AuthorRepository.java
+++ src/main/java/com/vishal/spring_database/repositories/AuthorRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface AuthorRepository extends CrudRepository<AuthorEntity, String> {
     Iterable<AuthorEntity> ageLessThan(int age);
 
-    @Query("SELECT a from Author a where a.age > ?1")
+    @Query("SELECT a from AuthorEntity a where a.age > ?1")
     Iterable<AuthorEntity> findAuthorsWithAgeGreaterThan(int age);
 }
