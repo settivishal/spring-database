d93b18f7c811b96474601bdb9b40a216b9f8e528
settivishal <vishalsetti2000@gmail.com>
2025-05-12 10:01:27 -0400

    Get books endpoint

--- src/main/java/com/vishal/spring_database/services/BookService.java
+++ src/main/java/com/vishal/spring_database/services/BookService.java
@@ -3,8 +3,12 @@ package com.vishal.spring_database.services;
 import com.vishal.spring_database.domain.entities.BookEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface BookService {
 
     BookEntity createBook(String isbn, BookEntity book);
+
+    List<BookEntity> findAll();
 }
